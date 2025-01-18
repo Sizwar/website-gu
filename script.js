fdde6bfe8c35b57e25600dfbb8c3f02522d26820
Sizwar <79133974+Sizwar@users.noreply.github.com>
2025-01-18 15:16:17 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,8 @@
 function showMessage() {
   alert('Terima kasih telah menghubungi saya!');
 }
+const toggleButton = document.getElementById('darkModeToggle');
+
+toggleButton.addEventListener('click', () => {
+  document.body.classList.toggle('dark-mode');
+});
